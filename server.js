181204b564e05b2b8de6a4294fc9ab41218789b0
ranjithreddy1996 <reddyyellareddys@gmail.com>
2020-02-20 14:26:36 +0530

    first commit

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/student/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8081, () => console.log(`Server started on port `));
